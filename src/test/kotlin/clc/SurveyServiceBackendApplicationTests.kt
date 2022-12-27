cd22dd729969d25374b5456b9049da09ff74ae1e
Michael Neuhold <S1810307094@students.fh-hagenberg.at>
2022-12-27 13:33:00 +0100

    init backend project

---
+++ src/test/kotlin/clc/SurveyServiceBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package clc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SurveyServiceBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

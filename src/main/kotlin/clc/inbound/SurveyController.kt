fa3a8b668689c5d19a3ce764295ef08cc9bb9401
Michael Neuhold <S1810307094@students.fh-hagenberg.at>
2022-12-29 16:45:54 +0100

    frontend -> backend communication ✅

--- src/main/kotlin/clc/inbound/SurveyController.kt
+++ src/main/kotlin/clc/inbound/SurveyController.kt
@@ -8,15 +8,11 @@ import clc.shared.mapper.DomainToDtoMapper
 import clc.shared.mapper.DtoToDomainMapper
 import org.bson.types.ObjectId
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/survey")
+@CrossOrigin(origins = ["*"])
 class SurveyController(private val surveyRepository: SurveyRepository) {
 
     @GetMapping

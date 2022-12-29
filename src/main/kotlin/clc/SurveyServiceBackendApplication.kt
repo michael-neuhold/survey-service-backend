04c99d059079602b2a6e51c9c7d746f794448b3b
Michael Neuhold <S1810307094@students.fh-hagenberg.at>
2022-12-29 13:11:05 +0100

    open api docs + swagger ui and postman collection

--- src/main/kotlin/clc/SurveyServiceBackendApplication.kt
+++ src/main/kotlin/clc/SurveyServiceBackendApplication.kt
@@ -4,8 +4,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 @EnableMongoRepositories
@@ -15,16 +13,3 @@ class SurveyServiceBackendApplication
 fun main(args: Array<String>) {
     runApplication<SurveyServiceBackendApplication>(*args)
 }
-
-@RestController
-class MessageResource {
-    @GetMapping("/")
-    fun index(): List<Message> = listOf(
-        Message("1", "Hello!"),
-        Message("2", "Bonjour!"),
-        Message("3", "Privet!"),
-        Message("4", "Hi Julian!"),
-    )
-}
-
-data class Message(val id: String?, val text: String)
